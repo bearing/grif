ac2e1ca6dd72853e96e51cc62f07b5f21f69c27d
neal <neal@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2010-08-24 17:23:59 +0000

    git-svn-id: file:///var/svn/grif/framework@74 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

---
+++ win32/trunk/source/gri_Loader.cpp
@@ -0,0 +1,16 @@
+#include "gri_Loader.h"
+
+GRILoader::GRILoader(GRIRegulator* regulator)
+{
+    this->regulator = regulator;
+}
+
+GRILoader::~GRILoader()
+{
+
+}
+
+GRIProcessObj* GRILoader::load(string process_name, string xml_file)
+{
+    return NULL;
+}
