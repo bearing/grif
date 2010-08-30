f2ed3a10703d9516c1dce839b01ca9ca2504c9f5
brunk <brunk@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2010-08-30 22:55:23 +0000

    cleaned up and deleted win32 folder
    git-svn-id: file:///var/svn/grif/framework@142 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/include/GRICLI.h
+++ trunk/include/GRICLI.h
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <QThread>
-#include "GRIDefines.h"
 
 class GRICommandLineInterface : public QThread
 {
@@ -17,6 +16,7 @@ public:
     int  InputCommand(); // obtains the next command from the user
     bool goodCommand(std::string command); //checks whether the user's command is defined
     void DisplayGoodbye(); // displays a goodbye message to the user on closing
+
     void closeInterface();
 
 signals:
--- trunk/include/GRIXMLParser.h
+++ trunk/include/GRIXMLParser.h
@@ -7,12 +7,8 @@
 #include <QFile>
 #include <QMessageBox>
 #include "GRIParamList.h"
-#include "GRIProcessThread.h"
 
 class GRIParamList;
-
-
-struct AnalysisStructureObject;
 
 class GRIXMLParser : public QWidget
 {
@@ -21,33 +17,12 @@ public:
 
     GRIParamList* readExampleAnalysisXMLFile();
 
-
-    /**
-      * reads filepath xml file and creates a list of ProcessDetails
-      * @returns list<ProcessDetails*>
-      * @see ProcessDetails
-      */
-    list<ProcessDetails*> readPathXML();
-    /**
-      * reads an xml file that details the analysis structure and creates a list of AnalysisStructureObjects
-      * @returns list<AnalysisStructureObject*>
-      */
-    list<AnalysisStructureObject*> readAnalysisStructureXML();
-    /**
-      * reads xml files and loads these lists into objects that can be read by the GRIRegulator
-      * @see readPathXML()
-      * @see readAnalysisStructureXML()
-      * @returns list<AnalysisStructureObject*>
-      */
-
-
-
-
 protected:
 
     GRIParamList* readParameter(QXmlStreamReader& xml, GRIParamList* head);
     void addElementToParam(QXmlStreamReader& xml, GRIParamList* param);
     void addChildParams(QXmlStreamReader& xml, GRIParamList* head);
+
 
     void pauseProgram(string message);
 
