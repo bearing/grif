961d59ec3ac8331ab6a78623d3214795cd832e9f
chivers <chivers@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2010-10-26 20:29:32 +0000

    Implemented IsLevelEnabled method and now places DEBUG, WARNING, and ERROR prefixes in log messages based on error level 1,2,and 3 resp.
    git-svn-id: file:///var/svn/grif/framework@291 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/source/GRILogMessage.cpp
+++ trunk/source/GRILogMessage.cpp
@@ -14,18 +14,58 @@ GRILogMessage::~GRILogMessage()
 }
 
 
-int GRILogMessage::SetMessageTime(QString s, int l)
+bool GRILogMessage::SetMessageTime(QString s, int l)
 {
-   // cout << "Set Log Message Time" << endl;
+
+
     SetTimeString();
-    MsgStr = DateTime + ": " + s;
-    cout << "Set Message Time: " << MsgStr.toStdString().c_str() << endl;
+    if (l == 1)
+        MsgStr = DateTime + " (" + ObjectName + ") " + ": DEBUG - " + s;
+    else if(l == 2)
+        MsgStr = DateTime + " (" + ObjectName + ") " + ": WARNING - " + s;
+    else if(l == 3)
+        MsgStr = DateTime + " (" + ObjectName + ") " + ": ERROR - " + s;
+    else if(l == 0)  // Just MSG
+        MsgStr = DateTime + " (" + ObjectName + ") " + ": " + s;
+    else
+        return 0;
 
     level = l;
-    return 0;
+    return 1;
 }
+
 
 void GRILogMessage::SetTimeString()
 {
-    DateTime = QDateTime(QDate::currentDate(),QTime::currentTime()).toString("ddd MMMM d,yyyy  hh:mm:ss.zzz");
+    DateTime = QDateTime(QDate::currentDate(),QTime::currentTime()).toString("dd.MM.yyyy hh:mm:ss.zzz");
+}
+
+bool GRILogMessage::IsLevelEnabled()
+{
+        QString GlobalLevelEnv = getenv("GRILOGLEVEL");
+        int GL;
+
+        if(GlobalLevelEnv == "VERBOSE") GL = 0;
+        else if(GlobalLevelEnv == "DEBUG") GL = 1;
+        else if(GlobalLevelEnv == "WARNING") GL = 2;
+        else if(GlobalLevelEnv == "ERROR") GL = 3;
+        else GL = 4;
+
+        return(level >= GL);
+
+}
+
+bool GRILogMessage::IsLevelEnabled(int l)
+{
+        QString GlobalLevelEnv = getenv("GRILOGLEVEL");
+        int GL;
+
+        if(GlobalLevelEnv == "VERBOSE") GL = 0;
+        else if(GlobalLevelEnv == "DEBUG") GL = 1;
+        else if(GlobalLevelEnv == "WARNING") GL = 2;
+        else if(GlobalLevelEnv == "ERROR") GL = 3;
+        else GL = 4;
+
+        return(l >= GL);
+
 }
