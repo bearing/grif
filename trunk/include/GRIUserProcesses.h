ea1bbb756cfde5c6c603f6b51b280350f3d60405
chivers <chivers@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2010-10-27 01:33:27 +0000

    GRIUserProcesses.h is empty at this point but will is a template (i.e. DO NOT OVERWRITE WITH PROJECT SPECIFIC STUFF) for including user process headers for the GRIUserLoader.load method.
    git-svn-id: file:///var/svn/grif/framework@295 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

---
+++ trunk/include/GRIUserProcesses.h
@@ -0,0 +1,15 @@
+#ifndef GRIUSERPROCESSES_H
+#define GRIUSERPROCESSES_H
+
+/* Users will include header files for their derived GRIDAQThread and
+   GRIAnalyisThread classes here.  This file will be included in the
+   GRIUserLoader.
+
+    Ex: #include "sisdaq.h"
+        #include "siscal.h"
+*/
+
+
+
+
+#endif // GRIUSERPROCESSES_H
