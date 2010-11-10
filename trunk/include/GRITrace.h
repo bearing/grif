72c4b81c35092a59c4e712c2d6ffee1d3b82d9ba
chivers <chivers@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2010-11-10 07:34:40 +0000

    git-svn-id: file:///var/svn/grif/framework@323 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/include/GRITrace.h
+++ trunk/include/GRITrace.h
@@ -4,20 +4,25 @@
 #include <QList>
 #include <QTime>
 
+// NOTE: Template classes must have implementation included in header file
+// i.e. No corresponding *.cpp
+// or else we get linking errors
+//
+
 template <class T>
 class GRITrace
 {
 
 public:
-    GRITrace();
-    ~GRITrace();
-    void ClearData();
-    void AddData(T ydata);
-    void AddData(QList<T> ydata);
-    void AddData(QList<T> ydata, QList<double> tdata);
-    void OffsetTime(bool yn);
-    QList<QTime> GetTData();
-    QList<T> GetYData();
+    GRITrace(){OffsetTimeData = false;}
+    ~GRITrace(){}
+    void ClearData(){}
+    void AddData(T ydata){}
+    void AddData(QList<T> ydata){}
+    void AddData(QList<T> ydata, QList<double> tdata){}
+    void OffsetTime(bool yn){this->OffsetTimeData = yn;}
+    QList<QTime> GetTData(){}
+    QList<T> GetYData(){}
 
 private:
     QList<T> tdata;
