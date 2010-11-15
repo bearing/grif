40cab03f437e23a001e0726ae1f9afd2d8071f30
chivers <chivers@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2010-11-15 04:10:28 +0000

    New GRIAnalysis implementation...
    git-svn-id: file:///var/svn/grif/framework@349 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/source/GRIAnalysisThread.cpp
+++ trunk/source/GRIAnalysisThread.cpp
@@ -1,41 +1,103 @@
 #include "GRIAnalysisThread.h"
 
-
 using namespace std;
 
 GRIAnalysisThread::GRIAnalysisThread()
 {
-
+    sleeping = false;
+    exitThreadFlag = false;
+    forceQuit = false;
 }
 
-GRIAnalysisThread::~GRIAnalysisThread()
-{
-    QThread::wait();
+GRIAnalysisThread::~GRIAnalysisThread(){
 }
 
-void GRIAnalysisThread::startAnalysis()
-{
-    start(QThread::NormalPriority);
+
+void GRIAnalysisThread::setExitThreadFlag(bool newExitThreadFlag){
+    if(sleeping){
+        sleeping = false;
+    //TODO:
+    //Tell regulator to unsleep thread.
+    }
+    exitThreadFlag = newExitThreadFlag;
 }
 
-void GRIAnalysisThread::stopAnalysis()
-{
-    QThread::quit();
+bool GRIAnalysisThread::getExitThreadFlag(){
+    return exitThreadFlag;
 }
 
-void GRIAnalysisThread::pauseAnalysis(unsigned long time)
-{
-    QThread::sleep(time);
+
+void GRIAnalysisThread::forceQuitAnalysis(){
+    forceQuit = true;
+    setExitThreadFlag(true);
 }
+
 
 void GRIAnalysisThread::run()
 {
+    cout << "GRIAnalysisThread run" << endl;
+    int error;
 
+    error = openInitializationControl();
+    if (error != ANALYSISTHREAD_SUCCESS) {
+        this->errorHandling("openInitializationControl failed", error);
+    }
+
+
+    error = initialize();
+    if(error != ANALYSISTHREAD_SUCCESS) {
+        this->errorHandling("initialize() failed", error);
+    }
+
+//    while(exitThreadFlag){
+//        error = openRunTimeControl();
+//        if(error != ANALYSISTHREAD_SUCCESS) {
+//            this->errorHandling("openRunTimeControl() failed", error);
+//        }
+
+
+        while(!exitThreadFlag){
+            error = Analyze();
+            if (error != ANALYSISTHREAD_SUCCESS){
+                this->errorHandling("Analyze() failed", error);
+            }
+            ReadGarbageCollection();  // All data read will be freed after each Analyze
+        }
+
+        cout << "exiting analysis" << endl;
+
+    }
+
+
+
+
+
+
+
+void GRIAnalysisThread::errorHandling(const char * message, int errorCode){
+    cerr << "GRIAnalysisThreadError: Message: " << message << endl;
+    cerr << "\tError code = " << errorCode << endl;
+    log << "GRIAnalysisThreadError: Message: " << message << endl;
+    log << "\tError code = " << errorCode << endl;
+    CommitLog(GRILOG_ERROR);
 }
 
-int GRIAnalysisThread::openInitializationControl()
+void GRIAnalysisThread::ReadGarbageCollection()
 {
-    return 0;
+    // Need to run GRIRegulator Garbage Collection for each pointer...
+    cout << "Analysis Thread Garbage Collection" << endl;
+    int nFree = reg->GarbageCollection(ReadDataPtrs);
+    if(nFree == ReadDataPtrs.size())
+        ReadDataPtrs.clear();
+    else
+        cerr << "! Garbage Collection Free Count not correct";
+
+
 }
+
+
+
+
+
 
 
