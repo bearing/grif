54120470dd97b753973112f94e629daae4aa5612
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2010-12-22 02:12:42 +0000

    GRIParser source file for new GRILoader
    git-svn-id: file:///var/svn/grif/framework@401 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

---
+++ trunk/source/GRIParser.cpp
@@ -0,0 +1,38 @@
+#include "GRIParser.h"
+
+
+bool GRIParser::parse(QString filePath){
+    QFile f(filePath);
+    QDomDocument doc;
+    QDomElement root, e;
+
+    doc.setContent(&f);
+
+    root = doc.documentElement();
+    e = root.firstChildElement("Name");
+    QDomAttr a = e.attributeNode("name");
+    this->className = a.value();
+
+    e = root.firstChildElement("ProcessDetails");
+
+    a = e.attributeNode("readerName");
+    this->readerName = a.value();
+
+    a = e.attributeNode("objectFromName");
+    this->objectFromName = a.value();
+
+
+    //run this over each element
+    e = root.firstChildElement("DataBlocks");
+    int num = e.elementsByTagName("Data").count();
+    e = e.firstChildElement("Data");
+    for(int i = 0; i < num; i++){
+        a = e.attributeNode("block");
+        this->dataBlockNames.push_back(a.value());
+        if(i != num-1){
+            e = e.nextSiblingElement("Data");
+        }
+    }
+
+    return true;
+}
