76c080a30d1d903c31f19744f7d5fda22b3e2c6b
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-02-04 04:24:19 +0000

    Added more error checking to xml parsing.
    git-svn-id: file:///var/svn/grif/framework@422 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/include/GRIParser.h
+++ trunk/include/GRIParser.h
@@ -24,61 +24,16 @@ class GRIParser{
 
 public:
 
-    void parse(QString filePath){
-        QFile f(filePath);
-        QDomDocument doc;
-        doc.setContent(&f);
-
-        cout << "Interpretation of file " << filePath.toStdString() << ":" << endl;
-        cout << doc.toString().toStdString() << endl;
-
-        QDomElement root = doc.documentElement();
-
-        QDomElement elem = root.firstChildElement("Objects");
-        int num = elem.elementsByTagName("object").count();
-        elem = elem.firstChildElement("object");
-
-        /* read the objects, constructing objectHash as we go along */
-        for(int i = 0; i < num; i++){
-            QDomAttr name = elem.attributeNode("name");
-            QDomAttr c = elem.attributeNode("class");
-            struct objectParsingDetails *opd = new struct objectParsingDetails;
-            opd->links = new std::list<struct linkParsingDetails*>;
-
-            opd->objectName = name.value();
-            opd->className = c.value();
-            objectHash[opd->objectName] = opd; //add to hash table
-            objectsAndLinks.push_back(*opd);
-
-
-            elem = elem.nextSiblingElement("object");
-
-        }
-
-        /* read the links */
-        QDomElement e = root.firstChildElement("Links");
-        num = e.elementsByTagName("link").count();
-        e = e.firstChildElement("link");
-
-        for(int i = 0; i < num; i++){
-            QDomAttr w = e.attributeNode("writer");
-            QDomAttr r = e.attributeNode("reader");
-            QDomAttr data = e.attributeNode("data");
-
-            struct linkParsingDetails *lpd = new struct linkParsingDetails;
-            lpd->writer = w.value();
-            lpd->reader = r.value();
-            lpd->dataBlock = data.value();
-
-            struct objectParsingDetails *opd2 = objectHash[lpd->writer];
-            if(opd2 != 0){
-                opd2->links->push_back(lpd);
-            }
-
-            e = e.nextSiblingElement("link");
-
-        }
-    }
+    /*
+     * parses the XML file path provided by filePath.  Sets up the
+     * objectsAndLinks data structure which contains the details
+     * of the parsing.  This data is used by GRILoader.  Use
+     * the getObjectsAndLinks() method to retrieve the data.
+     *
+     * parse() returns true if the parsing was successful and
+     * false otherwise
+     */
+    bool parse(QString filePath);
 
     /* returns the objects and links necessary for initRegulatorDetails() */
     std::list<struct objectParsingDetails> getObjectsAndLinks(){return objectsAndLinks;}
