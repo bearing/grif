0c3eec1b368fc44510b0027f11afa99a1d7dff14
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-02-04 04:24:36 +0000

    Added more error checking to xml parsing.
    git-svn-id: file:///var/svn/grif/framework@423 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/source/GRIParser.cpp
+++ trunk/source/GRIParser.cpp
@@ -1,37 +1,102 @@
 #include "GRIParser.h"
 
-
-/*bool GRIParser::parse(QString filePath){
+bool GRIParser::parse(QString filePath){
     QFile f(filePath);
+    if(!f.exists()){
+        cout << "ERROR: Could not find file: " << filePath.toStdString() << endl;
+        return false;
+    }
     QDomDocument doc;
-    QDomElement root, e;
+    if(!doc.setContent(&f)){
+        cout << "ERROR: Could not interpret file " << filePath.toStdString() << " as an xml file" << endl;
+        return false;
+    }
 
-    doc.setContent(&f);
+    cout << "Interpretation of file " << filePath.toStdString() << ":" << endl;
+    cout << doc.toString().toStdString() << endl;
 
-    root = doc.documentElement();
-    e = root.firstChildElement("Name");
-    QDomAttr a = e.attributeNode("name");
-    this->className = a.value();
+    QDomElement root = doc.documentElement();
 
-    e = root.firstChildElement("ProcessDetails");
+    QDomElement elem = root.firstChildElement("Objects");
+    if(elem.isNull()){
+        cout << "ERROR: Could not find the Objects tag. Please check your XML formatting." << endl;
+        return false;
+    }
+    int num = elem.elementsByTagName("object").count();
+    if(num == 0){
+        cout << "WARNING: file " << filePath.toStdString() << " has no objects declared." << endl;
+    }
+    elem = elem.firstChildElement("object");
 
-    a = e.attributeNode("readerName");
-    this->readerName = a.value();
-
-    a = e.attributeNode("objectFromName");
-    this->objectFromName = a.value();
-
-    //run this over each element
-    e = root.firstChildElement("DataBlocks");
-    int num = e.elementsByTagName("Data").count();
-    e = e.firstChildElement("Data");
+    /* read the objects, constructing objectHash as we go along */
     for(int i = 0; i < num; i++){
-        a = e.attributeNode("block");
-        this->dataBlockNames.push_back(a.value());
-        if(i != num-1){
-            e = e.nextSiblingElement("Data");
+        QDomAttr name = elem.attributeNode("name");
+        if(name.value().isNull()){
+            cout << "No object name found for an object.  Skipping that object..." << endl;
+            continue;
         }
+        QDomAttr c = elem.attributeNode("class");
+        if(c.value().isNull()){
+            cout << "No class name found for an object.  Skipping that object..." << endl;
+            continue;
+        }
+        struct objectParsingDetails *opd = new struct objectParsingDetails;
+        opd->links = new std::list<struct linkParsingDetails*>;
+
+        opd->objectName = name.value();
+        opd->className = c.value();
+        objectHash[opd->objectName] = opd; //add to hash table
+        objectsAndLinks.push_back(*opd);
+
+
+        elem = elem.nextSiblingElement("object");
+
+    }
+
+    /* read the links */
+    QDomElement e = root.firstChildElement("Links");
+    if(e.isNull()){
+        cout << "ERROR: Could not find the Links tag. Please check your XML formatting." << endl;
+        return false;
+    }
+    num = e.elementsByTagName("link").count();
+    if(num == 0){
+        cout << "WARNING: file " << filePath.toStdString() << " has no links declared." << endl;
+    }
+    e = e.firstChildElement("link");
+
+    for(int i = 0; i < num; i++){
+        QDomAttr w = e.attributeNode("writer");
+        if(w.value().isNull()){
+            cout << "No writer found for a link.  Skipping that link..." << endl;
+            continue;
+        }
+        QDomAttr r = e.attributeNode("reader");
+        if(r.value().isNull()){
+            cout << "No reader found for a link.  Skipping that link..." << endl;
+            continue;
+        }
+        QDomAttr data = e.attributeNode("data");
+        if(data.value().isNull()){
+            cout << "No data found for a link.  Skipping that link..." << endl;
+            continue;
+        }
+
+        struct linkParsingDetails *lpd = new struct linkParsingDetails;
+        lpd->writer = w.value();
+        lpd->reader = r.value();
+        lpd->dataBlock = data.value();
+
+        struct objectParsingDetails *opd2 = objectHash[lpd->writer];
+        if(opd2 != 0){
+            opd2->links->push_back(lpd);
+        }
+
+        e = e.nextSiblingElement("link");
+
     }
 
     return true;
-}*/
+
+}
+
