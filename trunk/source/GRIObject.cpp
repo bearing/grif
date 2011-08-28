5c45d4d6b4b9e4948600f181b9ffe10e00794ae8
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-08-28 23:33:02 +0000

    style update
    git-svn-id: file:///var/svn/grif/framework@553 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/source/GRIObject.cpp
+++ trunk/source/GRIObject.cpp
@@ -1,35 +1,21 @@
 #include "GRIObject.h"
 
-GRIObject::GRIObject()
-{
-
+GRIObject::GRIObject() {
      log.setString(&temp,QIODevice::ReadWrite);
-
 }
 
-GRIObject::~GRIObject()
-{
+GRIObject::~GRIObject() {}
 
-}
-
-void GRIObject::CommitLog(int level)
-{
-
-    if(LogMsg.IsLevelEnabled(level))
-    {
-
+void GRIObject::CommitLog(int level) {
+    if(LogMsg.IsLevelEnabled(level)) {
         if(LogMsg.SetMessageTime(log.readAll(),level))
-
             logSignal(LogMsg);
     } else {
         log.flush();
     }
-
-
 }
 
-void GRIObject::set_name(QString n){
-
+void GRIObject::set_name(QString n) {
     this->setObjectName(n);  //This should set the QObject name property
     LogMsg.SetObjectName(n);
 }
