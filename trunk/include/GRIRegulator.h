b21f82ab4c295e47af3f0b413cd8f4585a672bbf
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-08-28 23:30:00 +0000

    style update
    git-svn-id: file:///var/svn/grif/framework@541 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/include/GRIRegulator.h
+++ trunk/include/GRIRegulator.h
@@ -3,23 +3,16 @@
 
 #define REGULATOR_DEBUG
 
-#include <QWaitCondition>
-#include <QMutexLocker>
-#include <QTextStream>
-
-#include <QString>
-#include <string>
 #include <list>
 #include <iostream>
 #include <cassert>
 #include <utility>
 #include <QTime>
 #include <QMutex>
-
+#include <QString>
+#include <QWaitCondition>
 #include "GRILogMessage.h"
 #include "GRIObject.h"
-
-using namespace std;
 
 class GRILoader;
 class GRIDataBlock;
@@ -27,143 +20,128 @@ class GRIProcessThread;
 class GRIMemoryManager;
 class GRILogger;
 
-class GRIRegulator: public GRIObject
-{
+class GRIRegulator: public GRIObject {
+  friend class GRICommandAndControl;
+  friend class GRIDataBlock;
+  friend class GRIProcessThread;
 
-    //Q_OBJECT
+ public:
+  void reg_init();
+  GRIRegulator(GRIMemoryManager* ma);
+  ~GRIRegulator();
 
-    friend class GRICommandAndControl;
-    friend class GRIDataBlock;
-    friend class GRIProcessThread;
-    //friend class GRIDiagnosticsThread;
+  FILE *regulator_log;
+  GRILogger *logger;
+  QTime timer;
 
-public:
+  void setMemMgrPtr(GRIMemoryManager *managerPointer);
 
-    void reg_init();
-    //GRIRegulator(GRIMemoryManager* ma, GRILogger* logger);
-    GRIRegulator(GRIMemoryManager* ma);
+  /*
+   * initConfig() is called to initialize the whole system. It will require the
+   * dependencies' structure dictated by list<GRIBufferObject*> & list<GRIProcessThread*>
+   * and it will start (but not necessarily run) the thread.
+   */
+  void initConfig(std::list<GRIDataBlock*>* dataBlocks,
+                  std::list<GRIProcessThread*>* processes);
 
-    ~GRIRegulator();
+  /*
+   * start_threads() is called just before the whole system starts. It runs all the threads
+   */
+  void start_threads();
 
-    FILE *regulator_log;
+  /*
+   *
+   * bufferCreate() creates a buffer in the specified data block.
+   * If the data block does not exist yet, then it will create the data block before
+   * it creates the buffer.
+   *
+   * invariants:
+   * each process_name must be unique from all other process_names
+   * within each dataBlock, each bufferName must be unique.
+   *
+   */
+  bool bufferCreate(QString bufferName);
 
-    GRILogger *logger;
+  /*
+   * readMemory() reads one packet from memory in the location specified
+   * by process_name & bufferName
+   */
+  pair<int, char*> readMemory(QString blockName, QString bufferName);
 
-    QTime timer;
+  /*
+   *
+   * writeMemory() writes a data given in the char array to the location specified
+   * by process_name & bufferName
+   *
+   */
+  bool writeMemory(QString blockName, QString bufferName, int size, char dataArray[]);
 
-    void setMemMgrPtr(GRIMemoryManager *managerPointer);
-    /*
-     * initConfig() is called to initialize the whole system. It will require the
-     * dependencies' structure dictated by list<GRIBufferObject*> & list<GRIProcessThread*>
-     * and it will start (but not necessarily run) the thread.
-     */
+  /*
+   *
+   * currentPacketPosition() returns the current index of the packet marker. This is in most cases the last
+   * packet to be read next unless setPacketPosition() has been called.
+   *
+   */
+  int currentPacketPosition(QString bufferName);
 
-    void initConfig(list<GRIDataBlock*>* dataBlocks, list<GRIProcessThread*>* processes);
+  /*
+   *
+   * lastPacket() returns the index of the last packet in the specified buffer. This is equivalent to
+   * the buffer size minus one.
+   *
+   */
+  int lastPacket(QString bufferName);
 
-    /*
-     * start_threads() is called just before the whole system starts. It runs all the threads
-     */
-    void start_threads();
+  /*
+   *
+   * setPacketPosition() sets the packet marker for the specified buffer to the packetNumber position.
+   * This is useful for use with the overloaded readMemory function which allows users to read the packet
+   * that has been indexed by the packet marker. This is in most cases the last packet to be read from unless
+   * setPacketPosition() has been called.
+   *
+   * If the operation is successful, it returns true, otherwise false.
+   *
+   */
+  bool setPacketPosition(QString bufferName, int packetNumber);
 
-    /*
-     *
-     * bufferCreate() creates a buffer in the specified data block.
-     * If the data block does not exist yet, then it will create the data block before
-     * it creates the buffer.
-     *
-     * invariants:
-     * each process_name must be unique from all other process_names
-     * within each dataBlock, each bufferName must be unique.
-     *
-     */
-    bool bufferCreate(QString bufferName);
+  /*
+   * sizeofPacket() returns how big the packet is
+   */
+  int sizeofPacket(QString bufferName, int packetNumber);
 
-    /*
-     * readMemory() reads one packet from memory in the location specified
-     * by process_name & bufferName
-     */
-    pair<int, char*> readMemory(QString blockName, QString bufferName);
+  /*
+   * sizeofBuffer() returns how big the buffer is
+   */
+  int sizeofBuffer(QString bufferName);
 
-    /*
-     *
-     * writeMemory() writes a data given in the char array to the location specified
-     * by process_name & bufferName
-     *
-     */
-    bool writeMemory(QString blockName, QString bufferName, int size, char dataArray[]);
+  GRIMemoryManager* GetMemoryManager(){return mm;}
+  int GarbageCollection(QList<void*> pList);
 
-    /*
-     *
-     * currentPacketPosition() returns the current index of the packet marker. This is in most cases the last
-     * packet to be read next unless setPacketPosition() has been called.
-     *
-     */
-    int currentPacketPosition(QString bufferName);
+ protected:
+  GRIMemoryManager* mm;
 
-    /*
-     *
-     * lastPacket() returns the index of the last packet in the specified buffer. This is equivalent to
-     * the buffer size minus one.
-     *
-     */
-    int lastPacket(QString bufferName);
+ private:
+  QMutex GCMutex;
+  QMutex WriteMutex;
+  QMutex ReadMutex;
+  QMutex mutex;
+  QWaitCondition bufferIsReady;
 
-    /*
-     *
-     * setPacketPosition() sets the packet marker for the specified buffer to the packetNumber position.
-     * This is useful for use with the overloaded readMemory function which allows users to read the packet
-     * that has been indexed by the packet marker. This is in most cases the last packet to be read from unless
-     * setPacketPosition() has been called.
-     *
-     * If the operation is successful, it returns true, otherwise false.
-     *
-     */
-    bool setPacketPosition(QString bufferName, int packetNumber);
+  QList<char*> ReadDataPtrs;
+  int GarbageCollection(void* p);
 
-    /*
-     * sizeofPacket() returns how big the packet is
-     */
-    int sizeofPacket(QString bufferName, int packetNumber);
+  /*
+   * find_process() returns a pointer to the actual process given the name
+   */
+  GRIProcessThread* find_process(QString process_name);
 
-    /*
-     * sizeofBuffer() returns how big the buffer is
-     */
-    int sizeofBuffer(QString bufferName);
+  /*
+   * find_data() returns a pointer to the actual data block given the name
+   */
+  GRIDataBlock* find_data(QString data_block_name, QString buffer_name);
 
-    GRIMemoryManager* GetMemoryManager(){return mm;}
-    int GarbageCollection(QList<void*> pList);
-    // ***** Used for logging
-    //QTextStream log;
-    //void CommitLog(int level);
-    // *********
-protected:
-
-    GRIMemoryManager* mm;
-
-private:
-    QMutex GCMutex;
-    QMutex WriteMutex;
-    QMutex ReadMutex;
-    QMutex mutex;
-    QWaitCondition bufferIsReady;
-
-    QList<char*> ReadDataPtrs;
-    int GarbageCollection(void* p);
-
-    /*
-     * find_process() returns a pointer to the actual process given the name
-     */
-    GRIProcessThread* find_process(QString process_name);
-
-    /*
-     * find_data() returns a pointer to the actual data block given the name
-     */
-    GRIDataBlock* find_data(QString data_block_name, QString buffer_name);
-
-    list<GRIDataBlock*>* dataBlocks;
-
-    list<GRIProcessThread*>* processes;
-
+  std::list<GRIDataBlock*>* dataBlocks;
+  std::list<GRIProcessThread*>* processes;
 };
 
 #endif // GRIREGULATOR_H
