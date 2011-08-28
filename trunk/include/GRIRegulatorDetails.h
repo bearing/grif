48ba62413b97d4d788d41219b5977adcd6daa971
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-08-28 23:30:11 +0000

    style update
    git-svn-id: file:///var/svn/grif/framework@542 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/include/GRIRegulatorDetails.h
+++ trunk/include/GRIRegulatorDetails.h
@@ -4,29 +4,24 @@
 #include "GRIProcessThread.h"
 #include "GRIDataBlock.h"
 
-class GRIRegulatorDetails{
+class GRIRegulatorDetails {
+  friend class GRIProcessThread;
+  friend class GRIDataBlock;
 
-    friend class GRIProcessThread;
-    friend class GRIDataBlock;
+ public:
+  GRIRegulatorDetails(list<GRIProcessThread*> *processes, list<GRIDataBlock*> *data) {
+    this->processes = new list<GRIProcessThread*>();
+    this->processes = processes;
+    this->data = new list<GRIDataBlock*>();
+    this->data = data;
+  }
 
-public:
-    GRIRegulatorDetails(list<GRIProcessThread*> *processes, list<GRIDataBlock*> *data){
-        this->processes = new list<GRIProcessThread*>();
-        this->processes = processes;
-        this->data = new list<GRIDataBlock*>();
-        this->data = data;
-    }
+  list<GRIProcessThread*> *getProcesses(){return processes;}
+  list<GRIDataBlock*> *getData(){return data;}
 
-    list<GRIProcessThread*> *getProcesses(){return processes;}
-    list<GRIDataBlock*> *getData(){return data;}
-
-private:
-    list<GRIProcessThread*> *processes;
-    list<GRIDataBlock*> *data;
-
-
-
+ private:
+  list<GRIProcessThread*> *processes;
+  list<GRIDataBlock*> *data;
 };
-
 
 #endif // GRIREGULATORDETAILS_H
