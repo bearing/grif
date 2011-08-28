d9c1dfbd929d2ee40476b4ea3315fe5fd7b2a163
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-08-28 23:25:49 +0000

    style update
    git-svn-id: file:///var/svn/grif/framework@526 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/include/GRIClientSocket.h
+++ trunk/include/GRIClientSocket.h
@@ -1,16 +1,13 @@
 #ifndef GRI_CLIENTSOCKET_H
 #define GRI_CLIENTSOCKET_H
 
-#include <QtNetwork/qtcpsocket.h>
 #include <string>
+#include <QtNetwork/qtcpsocket.h>
 #include "GRIRunManager.h"
-
-using namespace std;
 
 class GRIRunManager;
 
-class GRIClientSocket : public QTcpSocket
-{
+class GRIClientSocket : public QTcpSocket {
     friend class GRIServer;
     friend class GRIRunManager;
     friend class GRIServerThread;
@@ -19,8 +16,7 @@ class GRIClientSocket : public QTcpSocket
 public:
     GRIClientSocket(QObject *parent = 0, int count = 0, GRIRunManager* = 0);
     ~GRIClientSocket();
-    string getInputWithSpaces();
-
+    std::string getInputWithSpaces();
 
 signals:
     void incomingCommand(QString command);
@@ -29,19 +25,15 @@ private slots:
     void readClient();
     void disconnectionMessage();
 
-
 protected:
     void sendData(list<string> output);
     void sendData(string output);
-
     void sendError();
 
     quint16 nextBlockSize;
-
     QString message;
     int id;
     GRIRunManager *manager;
-
 };
 
 #endif // CLIENTSOCKET_H
