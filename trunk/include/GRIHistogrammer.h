85a8edd75ac652ea9d5c424c6bc77adb3c580591
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-08-28 23:28:15 +0000

    style update
    git-svn-id: file:///var/svn/grif/framework@534 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/include/GRIHistogrammer.h
+++ trunk/include/GRIHistogrammer.h
@@ -1,9 +1,7 @@
 #ifndef GRIHISTOGRAMMER_H
 #define GRIHISTOGRAMMER_H
 
-#include "TH1D.h"
-#include "TH2D.h"
-#include "TH3D.h"
+#include "TH1.h"
 #include "GRIObject.h"
 #include <QString>
 
@@ -16,54 +14,50 @@
  * Note: this class has no associated .cpp file.
  */
 
-class GRIHistogrammer: public GRIObject
-{
-public:
-
+class GRIHistogrammer: public GRIObject {
+  public:
     // checking for readiness ... makes this pure abstract
-     virtual TH1* GetHist(){return 0;}
-     virtual int Clear() = 0;
+    virtual TH1* GetHist(){return 0;}
+    virtual int Clear() = 0;
 
-     virtual int GetID(){return ID;}
-     int GetDimension(){return dimension;}
+    virtual int GetID(){return ID;}
+    int GetDimension(){return dimension;}
 
-     int openInitializationControl(){return -1;}
-     void SetRateMode(bool tf){
-         rateModeFlag = tf;
-     }
-     bool GetRateMode(){return rateModeFlag;}
-     void SetPacketScaleFactor(double sf){packetScaleFactor = sf;}
-     double GetPacketScaleFactor(){return packetScaleFactor;}
+    int openInitializationControl() { return -1; }
+    void SetRateMode(bool tf) { rateModeFlag = tf; }
+    bool GetRateMode() { return rateModeFlag; }
+    void SetPacketScaleFactor(double sf) { packetScaleFactor = sf; }
+    double GetPacketScaleFactor() { return packetScaleFactor; }
 
-     QString GetName(){return HistName;}
-     QString GetBlockName(){return BlockName;}
+    QString GetName() { return HistName; }
+    QString GetBlockName() { return BlockName; }
 
-     bool isReady(){return BinSetFlag;}
+    bool isReady() { return BinSetFlag; }
 
-     void SetName(QString name){
-         this->HistName = name;
-         this->SetROOTHistName(name);
-     }
+    void SetName(QString name) {
+      this->HistName = name;
+      this->SetROOTHistName(name);
+    }
 
-     virtual void SetROOTHistName(QString name)=0;
+    virtual void SetROOTHistName(QString name) = 0;
 
 protected:
-    bool rateModeFlag;  // if true -> Updates histogram based on dHist*scale ...
+    // rateModeFlag:
+    // if true -> Updates histogram based on dHist*scale ...
     // if false -> Updates histogram directly (i.e. Ncnts total)
-    double packetScaleFactor;  // if Rate Mode then 1/Npacket is the normalizing factor
+    bool rateModeFlag;
+
+    // if Rate Mode then 1/Npacket is the normalizing factor
     //  Thus converges to rate over Npacket time
+    double packetScaleFactor;
 
     QString HistName;
     QString BlockName;
-
     bool BinSetFlag;
-
     int dimension; //the dimension of the histogram (1, 2, or 3)
     int ID; //id number of the histogrammer
 
-
 public:
-
     /*
      * The following are all dummy implementations.
      * They are implemented in GRIHist1D, GRIHist2D, and GRIHist3D
@@ -72,11 +66,9 @@ public:
      * GRIHist2D overrides the second Update()
      * GRIHist3D overrides the third Update()
      */
-
      virtual int Update(double x[], int numel){x = 0; numel = 0; return -1;}
      virtual int Update(double x[], double y[], int numel){x = y = 0; numel = 0; return -1;}
      virtual int Update(double x[], double y[], double z[], int numel){x = y = z = 0; numel = 0; return -1;}
-
 };
 
 #endif // GRIHISTOGRAMMER_H
