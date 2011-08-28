128efe92b9de713241134b6ca9a138c4254818b8
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-08-28 23:33:35 +0000

    style update
    git-svn-id: file:///var/svn/grif/framework@556 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/source/GRIThread.cpp
+++ trunk/source/GRIThread.cpp
@@ -1,34 +1,22 @@
 #include "GRIThread.h"
 
-GRIThread::GRIThread()
-{
-
+GRIThread::GRIThread() {
      log.setString(&temp,QIODevice::ReadWrite);
-
 }
 
-GRIThread::~GRIThread()
-{
+GRIThread::~GRIThread() {}
 
-}
-
-void GRIThread::CommitLog(int level)
-{
-
-    if(LogMsg.IsLevelEnabled(level))
-    {
-
-        if(LogMsg.SetMessageTime(log.readAll(),level))
-
+void GRIThread::CommitLog(int level) {
+    if(LogMsg.IsLevelEnabled(level)) {
+        if(LogMsg.SetMessageTime(log.readAll(),level)) {
             logSignal(LogMsg);
+        }
     } else {
         log.flush();
     }
-
 }
 
 void GRIThread::setObjectName(QString n){
-
     //this->setObjectName(n);
     this->setName(n.toStdString().c_str());  //This should set the QObject name property
     LogMsg.SetObjectName(n);
