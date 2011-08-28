b36b704e3f1e0bc239b3cd2c025ddfa887a6fb36
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-08-28 23:24:12 +0000

    style update
    git-svn-id: file:///var/svn/grif/framework@522 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/include/GRIAccumBuff.h
+++ trunk/include/GRIAccumBuff.h
@@ -1,88 +1,69 @@
 #ifndef GRIACCUMBUFF_H
 #define GRIACCUMBUFF_H
 
-#include <QVector>
 #include <QDateTime>
+#include <QVector>
 
 template <class T>
-        class GRIAccumBuff
-{
+class GRIAccumBuff {
+ public:
+  GRIAccumBuff() {
+    bubble = false;
+    LeadingEdge = false;
+  }
 
-public:
-    GRIAccumBuff()
-    {
-        bubble = false;
-        LeadingEdge = false;
+  ~GRIAccumBuff() {}
+
+  qint64 BufferData(int numel, qint64 t[], T d[]) {
+    qint64 dt = 0;
+    for (int i=0; i<numel; i++) {
+      if (t[i]>=t1 && t[i]<t2) {
+	time.append(t[i]);
+	data.append(d[i]);
+      }
+      // trigger on times greater than leading edge buffer
+      if (t[i] >= t2 && LeadingEdge) {
+	if (t[i]-t2 > dt) dt = t[i]-t2;  // time greater than leading edge
+      }
     }
-    ~GRIAccumBuff(){}
-    qint64 BufferData(int numel, qint64 t[], T d[])
-    {
-        qint64 dt = 0;
+    return dt;
+  }
 
-        for(int i=0; i<numel; i++)
-        {
-            if(t[i]>=t1 && t[i]<t2)
-            {
-                time.append(t[i]);
-                data.append(d[i]);
-                //QList::append()
+  void ResetBuffer(qint64 t1_, qint64 t2_) {
+    t1 = t1_;
+    t2= t2_;
+    data.clear();
+    time.clear();
+  }
 
-                //QListData::realloc()
-            }
+  bool isBubble() { return bubble; }
+  void SetBubble(bool tf) { bubble = tf; }
+  void SetLeadingEdge(bool tf) { LeadingEdge = tf; }
+  bool isLeadingEdge() { return LeadingEdge; }
 
-            // trigger on times greater than leading edge buffer
-            if(t[i] >= t2 && LeadingEdge){
-                if(t[i]-t2 > dt) dt = t[i]-t2;  // time greater than leading edge
-            }
+  T* DataArray() {
+    // Note the array is passed back and copied to the MM
+    // The deletion comes after the write statement(s) in GRIDAQAccumulator::Accumulate
+    T* a = new T[data.size()];
 
-        }
-
-        return dt;
+    for(int i=0; i < data.size(); i++) {
+      a[i] = data[i];
     }
+    return a;
+  }
 
-    void ResetBuffer(qint64 t1_, qint64 t2_)
-    {
-        t1 = t1_;
-        t2= t2_;
-        data.clear();
-        time.clear();
-    }
+  int GetDataSize() { return data.size(); }
 
-    bool isBubble(){return bubble;}
-    void SetBubble(bool tf){bubble = tf;}
-    void SetLeadingEdge(bool tf){LeadingEdge = tf;}
-    bool isLeadingEdge(){return LeadingEdge;}
+  qint64 GetT1() { return t1; }
+  qint64 GetT2() { return t2; }
 
-    T* DataArray(){
-        // Note the array is passed back and copied to the MM
-        // The deletion comes after the write statement(s) in GRIDAQAccumulator::Accumulate
-        T* a = new T[data.size()];
-
-        // typename QList<T>::iterator data_it;
-        // int ncnt = 0;
-
-        for(int i=0; i < data.size(); i++)
-        {
-            a[i] = data[i];
-        }
-        return a;
-    }
-
-    int GetDataSize(){return data.size();}
-
-    qint64 GetT1(){return t1;}
-    qint64 GetT2(){return t2;}
-
-private:
-    QList<T> data;  // list mode data
-    QList<qint64> time;  // list mode times
-    qint64 t1,t2;  // Timestamp boundaries of this buffer
-    bool bubble;  // Last buffer in times
-    bool LeadingEdge;  // First Buffer in times
-
-    T* dArray;
-
+ private:
+  QList<T> data;  // list mode data
+  QList<qint64> time;  // list mode times
+  qint64 t1,t2;  // Timestamp boundaries of this buffer
+  bool bubble;  // Last buffer in times
+  bool LeadingEdge;  // First Buffer in times
+  T* dArray;
 };
-
 
 #endif // GRIACCUMBUFF_H
