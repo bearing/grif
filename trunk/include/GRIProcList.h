d93d4b3179944eaafa901008f252388a282b4a68
augarten <augarten@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-10-01 20:45:01 +0000

    GRIProcList implemented
    git-svn-id: file:///var/svn/grif/framework@615 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

---
+++ trunk/include/GRIProcList.h
@@ -0,0 +1,76 @@
+#ifndef FRAMEWORK_TRUNK_INCLUDE_GRIPROCLIST_H_
+#define FRAMEWORK_TRUNK_INCLUDE_GRIPROCLIST_H_
+
+#include <QString>
+#include <QList>
+#include <QHash>
+#include "GRIProcessThread.h"
+/*
+ * Manages a DAQ process list and provides methods to
+ * modify each Processes local state during runtime.
+ */
+
+struct process {
+    QList<QString> sets_;
+    QList<QString> gets_;
+    QList<QString> run_actions_;
+};
+
+static const QString SETS = "sets";
+static const QString GETS = "gets";
+static const QString RUN_ACTIONS = "runactions";
+
+class GRIProcList {
+  public:
+    /*
+     * A new GRIProcList instantiated with the procList.
+     * \param procList the process list this manages
+     */
+    GRIProcList(QList<GRIProcessThread>*);
+
+    /*
+     * Inserts a type into the the specified process thread's blah blah blah,
+     * either sets, gets, or runActions.
+     * \return 1 upon successful insertion.
+     *      0 if element already exists. Note: it checks for duplicates and will not insert it.
+     *      -1 if the list cannot be found, or an error was produced.
+     */
+    int insertType(QString, QString, QString);
+    /*
+     * Deletes the type from the process thread's list thing.
+     * \return 1 upon successful deletion
+     *      0 if it finds the list but doesn't find the element to delete.
+     *      -1 if it can't find the list to delete from
+     */
+    int deleteType(QString, QString, QString);
+
+    /*
+     * Returns a QList of QStrings that represents the Sets of the process specified.
+     * Returns the empty list if there is no sets list associated with this process.
+     */
+    QList<QString> retrieveSets(QString);
+    /*
+     * Returns a QList of QStrings that represents the Gets of the process specified.
+     * Returns the empty list if there are no gets list associated with this process.
+     */
+    QList<QString> retrieveGets(QString);
+
+    /*
+     * Returns a QList of QStrings that represents the Sets of the process specified.
+     * Returns the empty list if there are no runactions list associated with this process.
+     */
+    QList<QString> retrieveRunActions(QString);
+
+  private:
+    /* The map of process name to process struct. */
+    QHash<QString, process*> process_map_;
+
+    /* Retrieves the list specified by QString from the given process. */
+    QList<QString>* getTypeList(process*, QString);
+
+    /* Retrueves the List associated with this process name. */
+    QList<QString>* retrieveList(QString, QString);
+};
+
+#endif  // FRAMEWORK_TRUNK_INCLUDE_GRIPROCLIST_H_
+
---
+++ trunk/source/GRIProcList.cpp
@@ -0,0 +1,70 @@
+#include "GRIProcList.h"
+
+//Public
+
+int GRIProcList::insertType(QString procName, QString type, QString value) {
+    process* proc = process_map_.value(procName);
+
+    if (proc == 0) {
+        process newproc;
+        proc = &newproc;
+        process_map_.insert(procName, proc);
+    }
+    QList<QString>* typeList = getTypeList(proc, type);
+    if (typeList == NULL) {
+        return -1;
+    } else if ((*typeList).contains(value)) {
+        return 0;
+    } else {
+       (*typeList).append(value);
+       return 1;
+    }
+}
+
+int GRIProcList::deleteType(QString procName, QString type, QString value) {
+    process* proc = process_map_.value(procName);
+    if (proc == 0) {
+        return -1;
+    } else {
+        QList<QString>* typeList = getTypeList(proc, type);
+        if (typeList == NULL) {
+            return -1;
+        } else if ((*typeList).removeOne(value)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+QList<QString> GRIProcList::retrieveSets(QString procName) {
+    return *retrieveList(procName, SETS);
+}
+QList<QString> GRIProcList::retrieveGets(QString procName) {
+    return *retrieveList(procName, GETS);
+}
+
+QList<QString> GRIProcList::retrieveRunActions(QString procName) {
+    return *retrieveList(procName, RUN_ACTIONS);
+}
+
+//Private
+QList<QString>* GRIProcList::getTypeList(process* proc, QString type) {
+    if (type.toLower().compare(SETS) == 0) {
+        return &(*proc).sets_;
+    } else if (type.toLower().compare(GETS) == 0) {
+        return &(*proc).gets_;
+    } else if (type.toLower().compare(RUN_ACTIONS) == 0) {
+        return &(*proc).run_actions_;
+    } else {
+        return NULL;
+    }
+}
+
+QList<QString>* GRIProcList::retrieveList(QString procName, QString list) {
+    process* proc = process_map_.value(procName);
+    if (proc != 0) {
+        return getTypeList(proc, list);
+    }
+    return new QList<QString>();
+}
