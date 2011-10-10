dd1a520257f7d4c8b2e05731949c6d7f51adb6e0
benson <benson@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-10-10 19:53:49 +0000

    support for code gen
    git-svn-id: file:///var/svn/grif/framework@653 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- trunk/source/GRIUserLoader.cpp
+++ trunk/source/GRIUserLoader.cpp
@@ -3,12 +3,9 @@
 // All possible process thread headers should be included from GRIUserProcesses.
 #include "GRIUserProcesses.h"
 
-GRIProcessThread *GRIUserLoader::load(QString class_name, QString instance_name) {
+GRIProcessThread *GRIUserLoader::load(QString class_name, QString object_name) {
   // process_name <=> class name
   // object_name  <=> object name
-
-  class_name = class_name.toLower();
-  instance_name = instance_name.toLower();
 
   // fall back null pointer
   GRIProcessThread *p = NULL;
