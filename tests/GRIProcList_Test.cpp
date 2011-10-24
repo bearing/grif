ad89169732c1d82139e52152d3bafc553a66c276
augarten <augarten@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-10-24 21:58:24 +0000

    Updated test file
    git-svn-id: file:///var/svn/grif/framework@662 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

--- tests/GRIProcList_Test.cpp
+++ tests/GRIProcList_Test.cpp
@@ -1,10 +1,9 @@
 #include <QString>
 #include <QtTest>
-#include "GRIProcList.h"
+#include "GRIProcList.cpp"
 
 class GRIProcList_Test : public QObject {
   Q_OBJECT
-
   public:
     GRIProcList_Test();
   private slots:
@@ -15,13 +14,26 @@ class GRIProcList_Test : public QObject {
     void retrievesRunActions();
 
   private:
+    GRIProcList* procList_;
 };
 
 GRIProcList_Test::GRIProcList_Test() {
+    procList_ = new GRIProcList();
 }
 
 void GRIProcList_Test::insertsType() {
+    QString* qs = new QString("process");
+    cout << "\n\n\n\nHello\n\n\n\n";
+    QString* setting = new QString("setting");
+    QVERIFY(1 == 1);
+    QVERIFY2((*procList_).insertType(*qs, GRIProcList::SETS, *setting) == 1,
+             "insertType should return 1 on successful insertion");
+    cout << "\n\n\n\nHello\n\n\n\n";
+    QList<QString>* sets = new QList<QString>();
+    (*sets).append("setting");
 
+    QVERIFY2((*procList_).retrieveSets(*(new QString("process"))).at(0).compare("setting") == 0,
+             "Should ");
 }
 void GRIProcList_Test::deletesType() {
     //Fill in with actual tests
--- tests/tests.pro
+++ tests/tests.pro
@@ -6,8 +6,31 @@ QT       += core network qt3support xml
 CONFIG += qtestlib
 TEMPLATE = app
 CONFIG += moc
-INCLUDEPATH += . \
-    ../trunk/include
+DEPENDPATH += .
+DEPENDPATH +=    ../trunk/include
+DEPENDPATH +=    ../trunk/source
 
+INCLUDEPATH += .
+INCLUDEPATH +=   ../trunk/include
+INCLUDEPATH +=   ../trunk/source
+
+
+#    ../../..trunk/include \
+#   ../../trunk/include \
+#    ../../../trunk/include \
+#    ../../trunk/include \
+
+#root headers
+ROOTDIR = /home/ben/Downloads/root
+INCLUDEPATH += $$ROOTDIR/include
+
+ROOTSYSLIB += $$ROOTDIR/lib
+INCLUDEPATH += $$ROOTSYSLIB
+LIBS += $$ROOTSYSLIB/libCint.so
+LIBS += $$ROOTSYSLIB/libMatrix.so
+LIBS += $$ROOTSYSLIB/libMathCore.so
+LIBS += -L/$$ROOTSYSLIB
+LIBS += -L$$ROOTSYS/lib -lCore -lHist
 # Input
 SOURCES += GRIProcList_Test.cpp
+
