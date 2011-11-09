15a8f92901937180553367602979846c22ddb734
augarten <augarten@d8f7f5d7-83b2-4a66-a7c0-045f5512b77b>
2011-11-09 20:27:16 +0000

    Added directions to main.cpp for adding tests
    git-svn-id: file:///var/svn/grif/framework@707 d8f7f5d7-83b2-4a66-a7c0-045f5512b77b

---
+++ tests/main.cpp
@@ -0,0 +1,22 @@
+#include "GRILogger_Test.cpp"
+#include "GRIProcList_Test.cpp"
+#include "QtTestUtil.h"
+#include "TestRegistry.cpp"
+
+
+/**
+  * This is the runner for all the test cases. In order to add a test, include the test file
+  * instantiate an instance of your test, and then add QTest::qExec(&[testobj], argc, arv)
+  * Separate tests for each class in a separate .cpp, naming convention is [ClassName]_Test.cpp
+  * The structure of each individual test class should be similar to GRIProcList_Test.cpp
+  * Be sure to register your test at the end and include the *.moc file.
+  */
+int main(int argc, char *argv[])
+{
+    GRILogger_Test tst;
+    GRIProcList_Test tst2;
+
+    QTest::qExec(&tst, argc, argv);
+    QTest::qExec(&tst2, argc, argv);
+    return 0;
+}
