60f1d191d17705eb292fa0796f61563f85e7c94a
Austin Benson <arbenson@berkeley.edu>
2011-11-14 09:39:41 -0600

    account for directory name changes

---
+++ examples/simulator/src/main.cpp
@@ -0,0 +1,39 @@
+#include <QtGui/QApplication>
+#include <QtCore/QCoreApplication>
+#include <QtGui/QtGui>
+#include <QtCore>
+#include <QLinkedList>
+#include <Qt>
+#include "GRICLI.h"
+#include "GRIDefines.h"
+#include "GRILoader.h"
+#include "GRIUserLoader.h"
+#include "GRIMemoryManager.h"
+#include "GRIRegulator.h"
+
+// TODO(arbenson): better description
+// Simulator example
+
+int main(int argc, char* argv[]) {
+  // the following will be included for all programs
+  QCoreApplication app(argc, argv);
+  GRIMemoryManager *mm = new GRIMemoryManager();
+  GRIRegulator *reg = new GRIRegulator(mm);
+
+  // create the list of files to use, this will be user-dependent
+  // (will be code-genned later)
+  QList<QString> files;
+  files.push_back("APP.XML");
+  // the following is necessary to set
+  QString rootXMLPath = "/Users/benson/Desktop/grif/examples/simulator/XML/app/";
+
+  // the following will be included for all programs
+  GRILoader *loader = new GRIUserLoader(rootXMLPath, reg, files);
+  loader->initRegulatorDetails();
+  reg->Start();
+  sleep(5);
+  reg->Stop();
+  sleep(10);
+  std::cout << "done..." << std::endl;
+  return app.exec();
+}
