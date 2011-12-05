67168a01aceae709102bb4e27997793460ace55a
Austin Benson <arbenson@berkeley.edu>
2011-12-04 22:01:20 -0600

    cleanup

--- examples/simulator/include/SIMDAQThread.h
+++ examples/simulator/include/SIMDAQThread.h
@@ -2,72 +2,57 @@
 #define SIMDAQTHREAD_H
 
 #include <cstdlib>
-#include "math.h"
-#include "time.h"
-#include "vector"
-#include "QVector"
-#include "GRIDAQBaseAccumNode.h"
-#include "GRIDAQThread.h"
-#include "GRIDataDefines.h"
+#include <math.h>
+#include <time.h>
+#include <QVector>
 #include "GRIDAQAccumulator.h"
-#include "GRIMath.h"
+#include "GRIDAQBaseAccumNode.h"
+#include "GRIDataDefines.h"
+#include "GRIDAQThread.h"
 
 class SIMDAQThread : public GRIDAQThread{
-
  public:
-
   SIMDAQThread(int num=1, double min=0, double max=65535, double rate=1);
   ~SIMDAQThread();
 
-  QVector<time_t> *t;
-
-  time_t lastTime;
-
-  int ID;
-
-  int init_ready;
-
-  void Setnchan(int numOfChans,double min=0, double max=65535, double rate=1);
-  int Getnchan(void);
-
-  void SetnPk(int numOfPks);
-  int GetnPk(void);
-
   int acquireData(int n);
 
-  QList<peak*> pk;
-  QList<background*> bkg;
-  QVector<dataOutput> simData;
-
-  GRIDAQBaseAccumNode* RegisterDataOutput(QString outName);
+  GRIDAQBaseAccumNode *RegisterDataOutput(QString outName);
 
   // No DAQ to connect to
-  int connectToDAQ() {
-    return 0;
-  }
+  int connectToDAQ() { return 0; }
+
   // not needed now
-  int initialize() {
-      return 0;
-  }
+  int initialize() { return 0; }
+
   // not needed now
-  int loadConfiguration() {
-      return 0;
-  }
+  int loadConfiguration() { return 0; }
+
   //Called at the beginning each run.
   int startDataAcquisition();
+
   //not needed now, called at the end of each run.
-  int stopDataAcquisition() {
-      return 0;
-  }
+  int stopDataAcquisition() { return 0; }
+
   int addPeak(double value, double sigma, double rate, int chan);
   int removePeak(int index);
+
+  void set_npk(int npk) { npk_ = npk; }
+  int get_npk() { return npk_; }
+
+  void init_chans(int numOfChans,double min=0, double max=65535, double rate=1);
+  int get_nchan() { return nchan_; }
 
  private:
   int npk_;
   int nchan_;
   QDateTime start_time_;
   QDateTime prev_time_;
-
+  QList<peak*> pk_;
+  QList<background*> bkg_;
+  bool init_ready_;
+  time_t last_time_;
+  QVector<time_t> times_;
 };
 
 #endif // SIMDAQTHREAD_H
