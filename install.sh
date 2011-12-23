e60a387a8a4aaa13419f39e7458cd612c5be47cd
Adam Midvidy <amidvidy@gmail.com>
2011-12-22 21:57:00 -0500

    basic idea done, need to fill in the details

--- install.sh
+++ install.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 # Installation script for the Gamma Ray Imaging Framerwork
+
+# Run this script in the directory in which you would like GRIF to be installed.
+# TODO (amidvidy): make this work for Linux and Windows (cygwin)
+
+which git
+if [ $? = 1 ]; 
+then
+    echo "Please install git before continuing."
+    exit
+fi
+
+echo "Pulling GRIF source from the github repository..."
+git clone git@github.com:bearing/grif.git
+
+echo -n "Do you have ROOT installed? (y/n)"
+read $ROOT
+if [ $ROOT = "y" ];
+then
+    echo "What is the path to your root directory?"
+    read $ROOTDIR
+else
+    # download and unpack ROOT binaries
+    $ROOTDIR=`pwd`
+fi
+
+# build GIT using the right environment variables.
+    
