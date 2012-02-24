a191d257b3f126336dcf507b4e47a72d0e44f1a9
Adam Midvidy <amidvidy@gmail.com>
2012-02-24 00:54:42 -0800

    started randgen

---
+++ examples/randgen/RandDAQThread.h
@@ -0,0 +1,10 @@
+#ifndef RANDDAQTHREAD_H
+#define RANDDAQTHREAD_H
+
+class RandDAQThread : public GRIDAQThread
+{
+public:
+  RandDAQThread();
+};
+
+#endif // RANDDAQTHREAD_H
---
+++ examples/randgen/RandDaqThread.cpp
@@ -0,0 +1,5 @@
+#include "RandDAQThread.h"
+
+RandDAQThread::RandDAQThread()
+{
+}
