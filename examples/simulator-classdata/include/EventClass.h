26f458ab759323bb3c3181d0af33b78fd0ebdb1f
Mark Bandstra <bandstra@berkeley.edu>
2012-04-20 19:39:31 -0700

    Changed EventClass to have fixed-length arrays so that size of class is known at compile time; this fixes all problems.

--- examples/simulator-classdata/include/EventClass.h
+++ examples/simulator-classdata/include/EventClass.h
@@ -4,6 +4,8 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
+#define MAX_LENGTH 20
+
 //#include <QList>
 #include <QVector>
 //#include <vector>
@@ -27,26 +29,34 @@ class EventClass {
   ~EventClass();
 
   void AddEnergy(double energy);
-  int NEnergies() { return energy_.size(); }
+//  int NEnergies() { return energy_.size(); }
+  int NEnergies() { return num_energy_; }
   double Energy(int i);
   double EnergyTest();
 
   void AddChan(int chan);
-  int NChan() { return chan_.size(); }
+//  int NChan() { return chan_.size(); }
+  int NChan() { return num_chan_; }
   int Chan(int i);
 
   void AddTS(qint64 ts);
-  int NTS() { return ts_.size(); }
+//  int NTS() { return ts_.size(); }
+  int NTS() { return num_ts_; }
   qint64 TS(int i);
 
  private:
 //  QList<double> energy_;
-  QVector<double> energy_;
+//  QVector<double> energy_;
 //  std::vector<double> energy_;
+  double energy_[MAX_LENGTH];
+  int num_energy_;
   double energy_test_;
-  QList<int> chan_;
-  QList<qint64> ts_;
-
+//  QList<int> chan_;
+  int chan_[MAX_LENGTH];
+  int num_chan_;
+//  QList<qint64> ts_;
+  qint64 ts_[MAX_LENGTH];
+  int num_ts_;
 
 };
 
--- examples/simulator-classdata/src/EventClass.cpp
+++ examples/simulator-classdata/src/EventClass.cpp
@@ -3,22 +3,37 @@
 #include "EventClass.h"
 
 EventClass::EventClass() {
-  energy_.clear();
+//  energy_.clear();
   energy_test_ = -1.0;
+//  chan_.clear();
+//  ts_.clear();
+  num_energy_ = 0;
+  num_chan_ = 0;
+  num_ts_ = 0;
 }
 
 EventClass::~EventClass() {
-  energy_.clear();
+//  energy_.clear();
   energy_test_ = -1.0;
+//  chan_.clear();
+//  ts_.clear();
+  num_energy_ = 0;
+  num_chan_ = 0;
+  num_ts_ = 0;
 }
 
 void EventClass::AddEnergy(double energy) {
-  energy_.push_back(energy);
+//  energy_.push_back(energy);
   energy_test_ = energy;
+  if (num_energy_<MAX_LENGTH) {
+    energy_[num_energy_] = energy;
+    num_energy_++;
+  }
 }
 
 double EventClass::Energy(int i) {
-  if (i<energy_.size()) {
+//  if (i<energy_.size()) {
+  if (i<num_energy_) {
     return energy_[i];
   }
   return -1.0;
@@ -29,22 +44,32 @@ double EventClass::EnergyTest() {
 }
 
 void EventClass::AddChan(int chan) {
-  chan_.push_back(chan);
+//  chan_.push_back(chan);
+  if (num_chan_<MAX_LENGTH) {
+    chan_[num_chan_] = chan;
+    num_chan_++;
+  }
 }
 
 int EventClass::Chan(int i) {
-  if (i<chan_.size()) {
+//  if (i<chan_.size()) {
+  if (i<num_chan_) {
     return chan_[i];
   }
   return -1;
 }
 
 void EventClass::AddTS(qint64 ts) {
-  ts_.push_back(ts);
+//  ts_.push_back(ts);
+  if (num_ts_<MAX_LENGTH) {
+    ts_[num_ts_] = ts;
+    num_ts_++;
+  }
 }
 
 qint64 EventClass::TS(int i) {
-  if (i<ts_.size()) {
+//  if (i<ts_.size()) {
+  if (i<num_ts_) {
     return ts_[i];
   }
   return qint64(-1);
